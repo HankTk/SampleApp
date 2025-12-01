6e390406bf518028b94d721df34871c1e796d54d
HankTk <hidenori@comcast.net>
2025-12-01 08:50:16 -0800

    Initial Commit

---
+++ SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by Hidenori Takaku on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SampleAppTests/SampleAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppTests.swift
+//  SampleAppTests
+//
+//  Created by Hidenori Takaku on 12/1/25.
+//
+
+import Testing
+@testable import SampleApp
+
+struct SampleAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
